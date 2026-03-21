7dd9e75acdebf573e8eba86f0bfd979dc884304b
Mateusz Wargacki <114600369+TeaSunflower@users.noreply.github.com>
2026-03-21 13:05:12 -0400

    Added Event Struct

---
+++ ConsoleVersionRYFGJ/ConsoleVersionRYFGJ/Event.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleVersionRYFGJ
+{
+    internal struct Event
+    {
+        private Item requiredStats;
+        private Item rewardItem;
+
+        public Item RequiredStats
+        {
+            get { return requiredStats; }
+        }
+
+        public Item RewardItem
+        {
+            get { return rewardItem; }
+        }
+
+        public Event(int _requiredDamage, int _requiredBlock, int _requiredMagic, string _rewardItemName, int _rewardItemDamage, int _rewardItemBlock, int _rewardItemMagic)
+        {
+            requiredStats = new Item(_requiredDamage, _requiredBlock, _requiredMagic);
+            rewardItem = new Item(_rewardItemName, _rewardItemDamage, _rewardItemBlock, _rewardItemBlock);
+        }
+    }
+}
--- ConsoleVersionRYFGJ/ConsoleVersionRYFGJ/Item.cs
+++ ConsoleVersionRYFGJ/ConsoleVersionRYFGJ/Item.cs
@@ -6,11 +6,20 @@ using System.Threading.Tasks;
 
 namespace ConsoleVersionRYFGJ
 {
+    /// <summary>
+    /// Item is a container class for item objects. It has 3 stats that will be used in encounters. 
+    /// </summary>
     internal struct Item
     {
+        private string name;
         private int damage;
         private int block;
         private int magic;
+
+        public string Name
+        {
+            get { return name; }
+        }
 
         public int Damage
         {
@@ -25,8 +34,17 @@ namespace ConsoleVersionRYFGJ
             get { return magic; }
         }
 
+        public Item(string _name, int _damage, int _block, int _magic)
+        {
+            name = _name;
+            damage = _damage;
+            block = _block;
+            magic = _magic;
+        }
+
         public Item(int _damage, int _block, int _magic)
         {
+            name = "No Name";
             damage = _damage;
             block = _block;
             magic = _magic;
