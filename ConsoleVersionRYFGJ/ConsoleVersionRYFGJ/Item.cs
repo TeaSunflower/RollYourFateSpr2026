b744bed29c2bc240d501acf06596ec33a6edefd0
Mateusz Wargacki <114600369+TeaSunflower@users.noreply.github.com>
2026-03-21 12:45:22 -0400

    Created Item Struct with 3 params

---
+++ ConsoleVersionRYFGJ/ConsoleVersionRYFGJ/Item.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleVersionRYFGJ
+{
+    internal struct Item
+    {
+        private int damage;
+        private int block;
+        private int magic;
+
+        public int Damage
+        {
+            get { return damage; }
+        }
+        public int Block
+        {
+            get { return block; }
+        }
+        public int Magic
+        {
+            get { return magic; }
+        }
+
+        public Item(int _damage, int _block, int _magic)
+        {
+            damage = _damage;
+            block = _block;
+            magic = _magic;
+        }
+    }
+}
